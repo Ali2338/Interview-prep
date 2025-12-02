d02858fdf71afa82353c4d71cf16d2ec00f19e01
Ali2338 <mohammadali98999899@gmail.com>
2025-12-02 19:54:04 +0530

    Fixed routing issue, switched to HashRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { BrowserRouter as Router,Routes,Route} from 'react-router-dom'
-import {Toaster} from 'react-hot-toast'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 import Login from './page/Auth/Login'
 import SignUp from './page/Auth/SignUp'
 import LandingPage from './page/LandingPage'
@@ -11,26 +11,26 @@ import UserProvider from './context/userContext'
 const App = () => {
   return (
     <UserProvider>
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/interview-prep/:sessionId" element={<InterviewPrep />} />
-        </Routes>
-      </Router>
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/interview-prep/:sessionId" element={<InterviewPrep />} />
+          </Routes>
+        </Router>
 
-      <Toaster
-      toastOptions={{
-        className: '',
-        style: {
-          fontSize: "13px",
-        },
-      }}/>
-
-    </div>
+        <Toaster
+          toastOptions={{
+            className: '',
+            style: {
+              fontSize: "13px",
+            },
+          }}
+        />
+      </div>
     </UserProvider>
   )
 }
